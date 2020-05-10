3e39c47d55168d42b7d82e69f98a8c7bcbcc2aab
Basile Henry <bjm.henry@gmail.com>
2020-05-10 20:38:23 +0200

    Add setup against onnxruntime source

---
+++ sys/README.md
@@ -0,0 +1,16 @@
+# onnxruntime-sys
+
+This crate contains the raw bindings to onnxruntime.
+
+In order to build/use this crate you must either have onnxruntime installed on
+your system or build the onnxruntime project from source.
+
+To build from source (Linux) do the following from the root of the repo:
+
+```bash
+./build.sh --config RelWithDebInfo --build_shared_lib --parallel
+export ONNXRUNTIME_LIB_DIR=$PWD/build/Linux/RelWithDebInfo/
+export ONNXRUNTIME_INCLUDE_DIR=$PWD/include/onnxruntime/core/session/
+export LD_LIBRARY_PATH=$ONNXRUNTIME_LIB_DIR:$LD_LIBRARY_PATH
+```
+
--- sys/build.rs
+++ sys/build.rs
@@ -2,12 +2,28 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    println!("cargo:rerun-if-env-changed=ONNXRUNTIME_LIB_DIR");
+    println!("cargo:rerun-if-env-changed=ONNXRUNTIME_INCLUDE_DIR");
+
+    match std::env::var("ONNXRUNTIME_LIB_DIR") {
+        Ok(path) => println!("cargo:rustc-link-search={}", path),
+        Err(_) => (),
+    };
+
+    let mut clang_args = String::new();
+    match std::env::var("ONNXRUNTIME_INCLUDE_DIR") {
+        Ok(path) => {
+            clang_args = format!("-I{}", path);
+        },
+        Err(_) => (),
+    };
+
     println!("cargo:rustc-link-lib=onnxruntime");
 
     let bindings = bindgen::Builder::default()
         .header("cbits/ort.h")
+        .clang_arg(clang_args)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // .whitelist_type("OrtApi")
         .whitelist_function("OrtGetApiBase")
         .whitelist_var("ORT_.*")
         .whitelist_recursively(true)
