f351c27ccf4239734ab44d29cc43a98abc8b2caa
Christopher Chalmers <c.chalmers@me.com>
2020-05-10 12:07:01 +0100

    Fix test

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -8,13 +8,15 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 pub fn check_status(api: &OrtApi, status: *mut OrtStatus) -> () {
     use std::ffi::CStr;
 
+
     let get_error_message = api.GetErrorMessage.unwrap();
     let release_status = api.ReleaseStatus.unwrap();
 
     unsafe {
         if !status.is_null() {
+
             let msg = CStr::from_ptr(get_error_message(status));
-            eprintln!("{}", msg.to_string_lossy());
+            eprintln!("error: {}", msg.to_string_lossy());
             release_status(status);
             exit(1);
         }
@@ -31,9 +33,6 @@ mod tests {
 
     #[test]
     fn mat_mul() -> Result<()> {
-        let model_path = CString::new("../../onnxruntime/test/testdata/matmul_1.onnx")
-            .unwrap()
-            .as_ptr();
 
         let api_base: &OrtApiBase = unsafe { OrtGetApiBase().as_ref().unwrap() };
 
@@ -42,45 +41,48 @@ mod tests {
             unsafe { get_api(ORT_API_VERSION).as_ref().unwrap() }
         };
 
-        let env: OrtEnv = {
+        let env: *mut OrtEnv = {
             let name = CStr::from_bytes_with_nul(b"test\0").unwrap().as_ptr();
             let create_env = api.CreateEnv.unwrap();
-            let mut raw_env = MaybeUninit::uninit();
+            let mut raw_env = std::ptr::null_mut();
             unsafe {
                 let status = create_env(
                     OrtLoggingLevel_ORT_LOGGING_LEVEL_WARNING,
                     name,
-                    &mut raw_env.as_mut_ptr(),
+                    &mut raw_env,
                 );
                 check_status(api, status);
-                raw_env.assume_init()
+                raw_env
             }
         };
 
-        let session_options: OrtSessionOptions = {
+        let session_options: *mut OrtSessionOptions = {
             let create_session_options = api.CreateSessionOptions.unwrap();
             let set_intra_op_num_threads = api.SetIntraOpNumThreads.unwrap();
-            let mut raw_session_options = MaybeUninit::uninit();
+            let mut raw_session_options = std::ptr::null_mut();
             unsafe {
-                let status = create_session_options(&mut raw_session_options.as_mut_ptr());
+                let status = create_session_options(&mut raw_session_options);
                 check_status(api, status);
-                set_intra_op_num_threads(raw_session_options.as_mut_ptr(), 1);
-                raw_session_options.assume_init()
+                set_intra_op_num_threads(raw_session_options, 1);
             }
+            raw_session_options
         };
 
-        let session: OrtSession = {
+        let model_path = CString::new("../../onnxruntime/test/testdata/matmul_1.onnx")
+            .unwrap();
+
+        let session: *mut OrtSession = {
             let create_session = api.CreateSession.unwrap();
-            let mut raw_session = MaybeUninit::uninit();
+            let mut raw_session = std::ptr::null_mut();
             unsafe {
                 let status = create_session(
-                    &env,
-                    model_path,
-                    &session_options,
-                    &mut raw_session.as_mut_ptr(),
+                    env,
+                    model_path.as_ptr(),
+                    session_options,
+                    &mut raw_session,
                 );
                 check_status(api, status);
-                raw_session.assume_init()
+                raw_session
             }
         };
 
